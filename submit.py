0f9347dbb2dd09e833170e5c731181686ccc4114
Anubhab Sen <anubhabsen@gmail.com>
2020-03-25 11:43:25 -0700

    fixes formats, tagger works 70% with overfit

--- submit.py
+++ submit.py
@@ -1,8 +1,9 @@
 import pycrfsuite
 from hw2_corpus_tool import *
+import sys
 
 
-ans = get_data('/Users/anubhabsen/Desktop/Spring 2020/NLP/Assignments/Assignment2/mini')
+ans = get_data('/Users/anubhabsen/Desktop/Spring 2020/NLP/Assignments/Assignment2/m_train')
 y_train = []
 X_train = []
 for i in range(len(ans)):
@@ -16,16 +17,13 @@ for i in range(len(ans)):
                 tokens.add(post.token)
                 pos_tags.add(post.pos)
         features = list(tokens) + list(pos_tags)
-        X_train.append(features)
-        y_train.append(j.act_tag)
-    #   break
-    # break
+        X_train.append([features])
+        y_train.append([j.act_tag])
 
 trainer = pycrfsuite.Trainer(verbose=False)
 
-# for i in range(len(X_train)):
-#     print(X_train[i], y_train[i])
-trainer.append(X_train, y_train)
+for i in range(len(X_train)):
+    trainer.append(X_train[i], y_train[i])
 
 trainer.set_params({
     'c1': 1.0,   # coefficient for L1 penalty
@@ -39,4 +37,14 @@ trainer.set_params({
 
 trainer.train('model.crfsuite')
 
-# tagger = pycrfsuite.Tagger()
+tagger = pycrfsuite.Tagger()
+tagger.open('model.crfsuite')
+print(tagger.tag(X_train[3]), y_train[3])
+tot = 0
+correct = 0
+for i in range(len(X_train)):
+	tot += 1
+	if tagger.tag(X_train[i])[0] == y_train[i][0]:
+		correct += 1
+
+print(tot, correct)
