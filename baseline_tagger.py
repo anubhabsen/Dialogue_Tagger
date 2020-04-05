dc6ac5c3a7ec57b7d844d93cf71c6113d1f35d56
Anubhab Sen <anubhabsen@gmail.com>
2020-04-04 23:35:28 -0700

    adds command line arguments as input

--- baseline_tagger.py
+++ baseline_tagger.py
@@ -2,8 +2,10 @@ import pycrfsuite
 from hw2_corpus_tool import *
 import sys
 
-
-ans = get_data('/Users/anubhabsen/Desktop/Spring 2020/NLP/Assignments/Assignment2/m_train')
+train_dir = sys.argv[1] # /Users/anubhabsen/Desktop/Spring 2020/NLP/Assignments/Assignment2/t_train
+test_dir = sys.argv[2]  # /Users/anubhabsen/Desktop/Spring 2020/NLP/Assignments/Assignment2/t_test
+out_file = sys.argv[3]  # predictions.txt
+ans = get_data(train_dir)
 y_train = []
 X_train = []
 X_test = []
@@ -40,7 +42,7 @@ for i in range(len(X_train)):
 trainer.set_params({
     'c1': 1.0,   # coefficient for L1 penalty
     'c2': 1e-3,  # coefficient for L2 penalty
-    'max_iterations': 150,  # stop earlier
+    'max_iterations': 50,  # stop earlier
 
     # include transitions that are possible, but not observed
     'feature.possible_transitions': True
@@ -49,7 +51,7 @@ trainer.set_params({
 
 trainer.train('model.crfsuite')
 
-test_data = get_data('/Users/anubhabsen/Desktop/Spring 2020/NLP/Assignments/Assignment2/m_test')
+test_data = get_data(test_dir)
 for i in range(len(test_data)):
     for index, j in enumerate(test_data[i]):
         tokens = set([])
@@ -78,9 +80,14 @@ tagger = pycrfsuite.Tagger()
 tagger.open('model.crfsuite')
 tot = 0
 correct = 0
+write_string = ""
 for i in range(len(X_test)):
-	tot += 1
-	if tagger.tag(X_test[i])[0] == y_test[i][0]:
-		correct += 1
+    tot += 1
+    write_string += str(tagger.tag(X_test[i])[0]) + "\n"
+    if tagger.tag(X_test[i])[0] == y_test[i][0]:
+        correct += 1
 
 print(correct / tot * 100)
+f = open(out_file, "w")
+f.write(write_string)
+f.close()
